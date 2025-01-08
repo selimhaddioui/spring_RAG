bf06457abe8f58a3024225eef6fdf33d625b3aee
lufroone <luc.labbe@efrei.net>
2025-01-08 11:37:24 +0100

    #2 deuxième route avec sample

--- src/main/java/fr/efrei/rag/web/rest/SampleResource.java
+++ src/main/java/fr/efrei/rag/web/rest/SampleResource.java
@@ -4,12 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.efrei.rag.web.dto.Sample;
+
 @RestController
 public class SampleResource {
 
     @GetMapping("/samples/{value}")
     public String hello(@PathVariable(value = "value", required=false) final String value) {
         return "Hello " + value + "!";
+    }
+
+    @GetMapping("/samples/dto/{value}")
+    public Sample helloDto(@PathVariable(value = "value", required=false) final String value) {
+        return new Sample("Hello " + value + "!");
     }
 
 }
